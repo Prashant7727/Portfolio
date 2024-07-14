f9631bf9a8f16e8fe9b024eed06b0fbe90325ff9
Prashant7727 <prashant.sisodia08@gmail.com>
2024-07-14 09:32:05 +0530

    changes in projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -8,6 +8,7 @@ import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
+// import E-Commerce from "../../Assets/Projects/E-Commerce.png"
 
 function Projects() {
   return (
@@ -25,10 +26,10 @@ function Projects() {
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="E-Commerce-Application"
+              description="An E-commerce application developed using React.js for the frontend, Node.js and Express.js for the backend, MongoDB for database management, and Bootstrap for responsive design."
+              ghLink="https://github.com/Prashant7727/E-Commerce-App"
+              demoLink="https://e-commerce-app-sigma-seven.vercel.app/"
             />
           </Col>
 
@@ -36,10 +37,10 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="File-Sharing-Application"
+              description="A file-sharing application developed using the MERN stack, designed to facilitate secure and efficient file transfers and management."
+              ghLink="https://github.com/Prashant7727/FileSharingApp"
+              // demoLink="https://blogs.soumya-jit.tech/"
             />
           </Col>
 
@@ -47,10 +48,10 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Task-Management-Application"
+              description="A task management application designed to streamline productivity and organization using modern web technologies. Developed with React.js for the frontend, Node.js and Express.js for the backend, MongoDB for database management, and Bootstrap for responsive design."
+              ghLink="https://github.com/Prashant7727/task-management-app"
+              // demoLink="https://editor.soumya-jit.tech/"              
             />
           </Col>
 
@@ -58,10 +59,10 @@ function Projects() {
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Email-Verification"
+              description="An application built to manage email verification processes using Node.js with Express.js for the backend and MongoDB for data storage."
+              ghLink="https://github.com/Prashant7727/email-verfictaion-using-nodemailer"
+              // demoLink="https://plant49-ai.herokuapp.com/"
             />
           </Col>
 
@@ -69,9 +70,9 @@ function Projects() {
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Weather-Application"
+              description="A weather application designed to provide current weather information and forecasts using modern web technologies. Developed with React.js for the frontend, Node.js and Express.js for the backend, and integrates with the OpenWeatherMap API for weather data."
+              ghLink="https://github.com/Prashant7727/Weather"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
@@ -80,10 +81,9 @@ function Projects() {
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              title="Myntra Clone Using Html and Css"
+              description="A clone of the Myntra e-commerce website designed using HTML and CSS to replicate its user interface and styling"
+              ghLink="https://github.com/Prashant7727/Myntra_Clone-using-html-and-css"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
           </Col>
